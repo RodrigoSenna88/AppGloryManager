b81856bf2781ff0e7a2625fcabb04351bad81239
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-03-02 14:25:00 -0300

    incluindo botões de criar conta e esqueci senha. configurando teclado e scrool da tela de sigin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,55 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform, ScrollView } from  'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container, Title, SubTitle } from './styles';
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import {
+  Container,
+  Title,
+  SubTitle,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Title>Glory Manager</Title>
-      <SubTitle>Faça seu logon</SubTitle>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex:1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined }
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1}}
+        >
+          <Container>
+              <Title>Glory Manager</Title>
+
+              <SubTitle>Faça seu logon</SubTitle>
+
+            <Input name="email" icon="mail" placeholder="E-mail"/>
+            <Input name="password" icon="lock" placeholder="Senha"/>
+
+            <Button onPress={() => {}}>Entrar</Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#941FBD" />
+        <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 }
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
@@ -19,3 +20,37 @@ export const SubTitle = styled.Text`
   margin: 64px 0 24px;
 
 `;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #941FBD;
+  font-size: 18px;
+  font-family: 'Robotoslab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #E6B9F6;
+  border-top-width: 1px;
+  border-color: #941FBD;
+  padding: 16px 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #941FBD;
+  font-size: 18px;
+  font-family: 'Robotoslab-Medium';
+  margin-left: 16px;
+`;
+
