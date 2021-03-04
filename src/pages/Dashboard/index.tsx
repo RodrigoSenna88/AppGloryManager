5d84f7dfe8e16864e14b3737e872c38d7763f9ce
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-03-04 16:54:15 -0300

    Terminando página de dasboard(estática)

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-expressions */
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import Button from '../../components/Button';
 
 import {
   Container,
@@ -9,6 +12,15 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  StudentList,
+  StudentContainer,
+  StudentInfo,
+  StudentName,
+  StudentScheduleIcon,
+  StudentSchedule,
+  LogOutButton,
+  LogOutButtonText,
+  ButtonContainer,
 } from './styles';
 
 const Dashboard: React.FC = () => {
@@ -18,27 +30,81 @@ const Dashboard: React.FC = () => {
     navigate('Profile');
   }, [navigate]);
   return (
-    <Container>
-      <Header>
-        <HeaderTitle>
-          Olá, {'\n'}
-          <UserName>Rodrigo Senna</UserName>
-        </HeaderTitle>
+    <>
+      <Container>
+        <Header>
+          <HeaderTitle>
+            Olá, {'\n'}
+            <UserName>Rodrigo Senna</UserName>
+          </HeaderTitle>
 
-        <ProfileButton
-          onPress={() => {
-            navigateToProfile;
-          }}
-        >
-          <UserAvatar
-            source={{
-              uri:
-                'https://avatars.githubusercontent.com/u/56227526?s=400&u=2074ddcc5a2198c3778cdc643518afcd5ee67c7e&v=4',
+          <ProfileButton
+            onPress={() => {
+              navigateToProfile;
             }}
-          />
-        </ProfileButton>
-      </Header>
-    </Container>
+          >
+            <UserAvatar
+              source={{
+                uri:
+                  'https://avatars.githubusercontent.com/u/56227526?s=400&u=2074ddcc5a2198c3778cdc643518afcd5ee67c7e&v=4',
+              }}
+            />
+          </ProfileButton>
+        </Header>
+
+        <StudentList>
+          <StudentContainer
+            onPress={() => {
+              console.log('foi');
+            }}
+          >
+            <StudentInfo>
+              <StudentName>Rodrigo Senna</StudentName>
+
+              <StudentScheduleIcon>
+                <Icon name="clock" size={14} color="#B776B0" />
+
+                <StudentSchedule>Manhã</StudentSchedule>
+              </StudentScheduleIcon>
+            </StudentInfo>
+          </StudentContainer>
+
+          <StudentContainer
+            onPress={() => {
+              console.log('foi');
+            }}
+          >
+            <StudentInfo>
+              <StudentName>Rafael Senna</StudentName>
+
+              <StudentScheduleIcon>
+                <Icon name="clock" size={14} color="#B776B0" />
+
+                <StudentSchedule>Tarde</StudentSchedule>
+              </StudentScheduleIcon>
+            </StudentInfo>
+          </StudentContainer>
+        </StudentList>
+        <ButtonContainer>
+          <Button
+            onPress={() => {
+              console.log('foi');
+            }}
+          >
+            Criar nova matrícula
+          </Button>
+        </ButtonContainer>
+      </Container>
+
+      <LogOutButton
+        onPress={() => {
+          console.log('foi');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#941FBD" />
+        <LogOutButtonText>Sair da aplicação</LogOutButtonText>
+      </LogOutButton>
+    </>
   );
 };
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,4 @@
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -14,7 +15,7 @@ export const Header = styled.View`
 `;
 
 export const HeaderTitle = styled.Text`
-  color: #fff;
+  color: #b776b0;
   font-size: 24px;
   font-family: 'RobotoSlab-Regular';
   line-height: 28px;
@@ -31,4 +32,70 @@ export const UserAvatar = styled.Image`
   width: 56px;
   height: 56px;
   border-radius: 28px;
+`;
+
+export const StudentList = styled.View`
+  padding: 32px 24px 16px;
+`;
+
+export const StudentContainer = styled(RectButton)`
+  background: #e6b9f6;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 16px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const StudentInfo = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StudentName = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  color: #941fbd;
+`;
+
+export const StudentScheduleIcon = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+export const StudentSchedule = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  color: #b776b0;
+  margin-left: 5px;
+`;
+
+export const LogOutButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #e6b9f6;
+  border-top-width: 1px;
+  border-color: #941fbd;
+  padding: 16px 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const LogOutButtonText = styled.Text`
+  color: #941fbd;
+  font-size: 18px;
+  font-family: 'Robotoslab-Medium';
+  margin-left: 16px;
+`;
+
+export const ButtonContainer = styled.View`
+  padding: 20px;
 `;
